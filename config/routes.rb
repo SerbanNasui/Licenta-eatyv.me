7380012873e0bac76bae791b1b9e6217c0364939
SerbanNasui <serban.nasui@gmail.com>
2019-06-19 18:29:50 +0300

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  default_url_options :host => "localhost:3000"
   ActiveAdmin.routes(self)
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
